9c16abbc39cd956f109ceb60a72b1323e7a1bb13
Colin Campbell <colin.campbell@digitalistgroup.com>
2024-10-29 13:57:39 +0100

    Use our own repo for testing

--- install.js
+++ install.js
@@ -4,7 +4,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/CharafChnioune/MFLUX-WEBUI.git app",
+          "git clone -b fix-guidance-and-premature-var-deletion https://github.com/colin-campbell/MFLUX-WEBUI.git app",
         ]
       }
     },
